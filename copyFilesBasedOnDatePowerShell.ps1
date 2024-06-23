ea8aff4a39814d50fb4d5e21cb3b670470d59a6c
helenapath <44349347+helenapath@users.noreply.github.com>
2024-06-23 18:56:09 -0400

    Update copyFilesBasedOnDatePowerShell.ps1

--- copyFilesBasedOnDatePowerShell.ps1
+++ copyFilesBasedOnDatePowerShell.ps1
@@ -1,21 +1,16 @@
-#1) Define source folder to copy data from
-#2) Define destination folder to copy data to
-#3) Defined the create date range of the files to be copied
-#4) Identify files in source folder that matches the date criteria
-#5) Recurse through the list of files to be copied and copy file
-#1) Define source folder to copy data from
-#2) Define destination folder to copy data to
-#3) Defined the create date range of the files to be copied
-#4) Identify files in source folder that matches the date criteria
+#1) Define the source and destination folder to copy data to/from.
+#2) Defined the create date range of the files to be copied using get-date cmdlet.
+#4) Identify files in source folder that matches the date criteria.
 #5) Recurse through the list of files to be copied and copy file
 
 Write-Host "Script to Move Files Based on Date"
-$source = "C:\Helena\Resume\2024\Job Applications 2024\1Knowledge Articles\"
-$destination = "C:\Helena\Resume\2024\Job Applications 2024\1Knowledge Articles\test"
-$DateGrThan = "06/21/2024"
-$DateLessThan = "06/30/2024"
+$Source = "C:\Source\"
+$Destination = "C:\Destination"
+$StartDate =get-date.day
+$EndDate =$date.adddays(-2)
 
-$Files = Get-ChildItem -Path $source -File |Where-Object { $_.CreationTime -ge $DateGrThan  -and $_.CreationTime -le $DateLessThan  }
+
+$Files = Get-ChildItem -Path $source -File |Where-Object { $_.CreationTime -ge $StartDate  -and $_.CreationTime -le $EndDate  }
 
 #recurse through each file that meets the criteria
 foreach ($File in $Files)

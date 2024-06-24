61176d3a9cdecafe56531ba39a3ff1b8474f1255
helenapath <44349347+helenapath@users.noreply.github.com>
2024-06-24 11:40:56 -0400

    Update copyFilesBasedOnDatePowerShell.ps1

--- copyFilesBasedOnDatePowerShell.ps1
+++ copyFilesBasedOnDatePowerShell.ps1
@@ -1,13 +1,14 @@
 #1) Define the source and destination folder to copy data to/from.
 #2) Defined the create date range of the files to be copied using get-date cmdlet.
 #4) Identify files in source folder that matches the date criteria.
-#5) Recurse through the list of files to be copied and copy file
+#5) Recurse through the list of files to be copied and copy file. Throw error if file is not copied
 
 Write-Host "Script to Move Files Based on Date"
 $Source = "C:\Source\"
 $Destination = "C:\Destination"
-$StartDate =get-date.day
-$EndDate =$date.adddays(-2)
+$date = Get-Date
+$StartDate = $Date.day
+$EndDate =$Date.adddays(-2)
 
 
 $Files = Get-ChildItem -Path $source -File |Where-Object { $_.CreationTime -ge $StartDate  -and $_.CreationTime -le $EndDate  }
@@ -15,8 +16,14 @@ $Files = Get-ChildItem -Path $source -File |Where-Object { $_.CreationTime -ge $
 #recurse through each file that meets the criteria
 foreach ($File in $Files)
 {
-	copy-item $File $destination
-	write-host "copying $File"
-} 
+	try
+		{
+			copy-item $File $destination
+			write-Host "Copied $File Success"
+		} 
 
-
+	Catch
+		{
+			write-Host "Copy $File Failure"
+		}
+} #End of For loop
